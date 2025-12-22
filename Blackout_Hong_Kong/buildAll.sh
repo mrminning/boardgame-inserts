d72556bbed709bebd2e0f2156b99b9da2a63528e
mrminning <sven@minning.se>
2025-12-22 07:13:44 +0100

    Blackout Hong Kong

---
+++ Blackout_Hong_Kong/buildAll.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+include="include<main.scad>;\n"
+
+for result in $(grep module main.scad | sed 's/module //' | sed 's/ {/;/');
+do
+    filename=$(echo $result | sed 's/();/.scad/');
+    #if [[ $filename == "module"* ]]; then
+        echo $include > $filename;
+        echo $result >> $filename;
+    #fi
+done
+
+make -f Makefile
